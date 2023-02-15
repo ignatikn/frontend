d1b720ac6236ea4f4900eefd5b1b1f5006814267
ignatikn <ignatik.1580@mail.ru>
2023-02-16 00:50:09 +0700

    адаптивная верстка полностью

---
+++ src/scripts/dev/main.js
@@ -0,0 +1,5 @@
+const root = document.documentElement;
+const navToggle = document.querySelector("#js-navToggle");
+navToggle.addEventListener("click", function () {
+  root.classList.toggle("show-nav");
+});
