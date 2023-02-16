82d4c8f6f371899e399b495608b44051afaaf26d
ignatikn <ignatik.1580@mail.ru>
2023-02-16 07:16:26 +0700

    add upcomming

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,3 +3,24 @@ const navToggle = document.querySelector("#js-navToggle");
 navToggle.addEventListener("click", function () {
   root.classList.toggle("show-nav");
 });
+
+const swipers = document.querySelectorAll(".js-swiper");
+swipers.forEach(function (swpr) {
+  new Swiper(swpr, {
+    updateOnWindowResize: true,
+    slidesPerView: "auto",
+    freeMode: true,
+    spaceBetween: 0,
+    speed: 500,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-arrow-next",
+      prevEl: ".swiper-arrow-prev",
+      disabledClass: "arrow--disabled"
+    }
+  });
+});
