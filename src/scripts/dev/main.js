50784a5a8b9d9eb625c758f1bbca567becfa2cd9
ignatikn <ignatik.1580@mail.ru>
2023-02-24 08:56:31 +0700

    contacts add

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -25,3 +25,23 @@ swipers.forEach(function (swpr) {
     }
   });
 });
+
+const contactsMap = document.querySelector("#js-contactsMap");
+if (contactsMap) {
+  const mapStyles = []; //здесь должен быть задан JSON со стилями
+  const mapCenter = new google.maps.LatLng(56.49387, 84.96274);
+  const mapOptions = {
+    center: mapCenter,
+    disableDefaultUI: true,
+    draggable: true,
+    gestureHandling: "cooperative",
+    scrollwheel: false,
+    styles: mapStyles,
+    zoom: 15,
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_BOTTOM
+    }
+  };
+  const map = new google.maps.Map(contactsMap, mapOptions);
+}
