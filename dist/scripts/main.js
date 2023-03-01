e305a345bdd465471e537588583cd68e046b1073
ignatikn <ignatik.1580@mail.ru>
2023-03-02 04:30:31 +0700

    попап

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -45,3 +45,32 @@ if (contactsMap) {
   };
   const map = new google.maps.Map(contactsMap, mapOptions);
 }
+
+const eventPP = document.querySelector("#js-eventPP");
+  if (eventPP) {
+    const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
+    const closeEventPP = function (event) {
+      function close() {
+        document.removeEventListener("keyup", closeEventPP);
+        eventPP.removeEventListener("click", closeEventPP);
+        root.classList.remove("show-event-popup");
+      }
+      switch (event.type) {
+        case "keyup":
+          if (event.key === "Escape" || event.keyCode === 27) close();
+          break;
+        case "click":
+          if (
+            event.target === this ||
+            event.target.classList.contains("js-ppCloseBtn")
+          )
+            close();
+          break;
+      }
+    };
+    eventOpenBtn.addEventListener("click", function () {
+      root.classList.add("show-event-popup");
+      document.addEventListener("keyup", closeEventPP);
+      eventPP.addEventListener("click", closeEventPP);
+    });
+  }
